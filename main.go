9a419112dd5fdcfcb030d0d0cc1a5c0452199d85
egsam98 <egsam98@gmail.com>
2020-08-11 14:11:06 +0300

    реализация собственного обработчика ошибок во время ответа gRPC -> HTTP

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/status"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -15,7 +17,7 @@ var PORT = os.Getenv("PORT")
 func main() {
 	logger := initLogger()
 
-	mux := runtime.NewServeMux()
+	mux := runtime.NewServeMux(runtime.WithProtoErrorHandler(errorHandler))
 	err := rusprofile2.RegisterServiceHandlerServer(context.Background(), mux, rusprofile2.NewRusprofileServer(logger))
 	if err != nil {
 		panic(err)
@@ -31,4 +33,17 @@ func initLogger() grpclog.LoggerV2 {
 		panic(err)
 	}
 	return grpclog.NewLoggerV2(os.Stdout, ioutil.Discard, f)
+}
+
+func errorHandler(_ context.Context, _ *runtime.ServeMux, _ runtime.Marshaler, w http.ResponseWriter, _ *http.Request, err error) {
+	st := status.Convert(err)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(runtime.HTTPStatusFromCode(st.Code()))
+	err = json.NewEncoder(w).Encode(map[string]interface{}{
+		"error":     st.Message(),
+		"gRPC-code": st.Code(),
+	})
+	if err != nil {
+		w.Write([]byte(`{"error": "failed to marshal error"}`))
+	}
 }
